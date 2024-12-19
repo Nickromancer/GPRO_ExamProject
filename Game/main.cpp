cf459dc1c4507cebce17ae6290bd5f58b733b5c4
SandwichShoes <162912355+SandwichShoes@users.noreply.github.com>
2024-12-19 22:29:34 +0100

    Create NutNote component, test setup for checking LERP movement

--- Game/main.cpp
+++ Game/main.cpp
@@ -6,6 +6,7 @@
 #include "Game/ComponentMusic.h"
 #include "Game/ComponentNode.h"
 #include "Game/ComponentBackground.h"
+#include "Game/ComponentNutNote.h"
 
 
 MyEngine::Engine engine;
@@ -14,6 +15,7 @@ int main() {
 	MyEngine::ComponentFactory::RegisterComponentOfType("MUSIC", []() {return std::make_shared<ComponentMusic>();  });
 	MyEngine::ComponentFactory::RegisterComponentOfType("NODE", []() {return std::make_shared<ComponentNode>();  });
 	MyEngine::ComponentFactory::RegisterComponentOfType("BACKGROUND", []() { return std::make_shared<ComponentBackground>(); });
+	MyEngine::ComponentFactory::RegisterComponentOfType("NUTNOTE", []() { return std::make_shared<ComponentNutNote>(); });
 
 	engine.Init("data/scene.json");
 }
