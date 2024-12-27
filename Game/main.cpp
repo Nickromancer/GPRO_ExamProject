f5d283bb53d34a4e5404ec93d7ef1da81ea982dc
SandwichShoes <162912355+SandwichShoes@users.noreply.github.com>
2024-12-27 22:44:22 +0100

    PlayerControllerComponent added

--- Game/main.cpp
+++ Game/main.cpp
@@ -7,15 +7,27 @@
 #include "Game/ComponentNode.h"
 #include "Game/ComponentBackground.h"
 #include "Game/ComponentNutNote.h"
+<<<<<<< Updated upstream
+=======
+#include "Game/ComponentPlayerController.h"
+>>>>>>> Stashed changes
 
 
 MyEngine::Engine engine;
 
 int main() {
+<<<<<<< Updated upstream
 	MyEngine::ComponentFactory::RegisterComponentOfType("MUSIC", []() {return std::make_shared<ComponentMusic>();  });
 	MyEngine::ComponentFactory::RegisterComponentOfType("NODE", []() {return std::make_shared<ComponentNode>();  });
 	MyEngine::ComponentFactory::RegisterComponentOfType("BACKGROUND", []() { return std::make_shared<ComponentBackground>(); });
 	MyEngine::ComponentFactory::RegisterComponentOfType("NUTNOTE", []() { return std::make_shared<ComponentNutNote>(); });
+=======
+    MyEngine::ComponentFactory::RegisterComponentOfType("MUSIC", []() {return std::make_shared<ComponentMusic>();  });
+    MyEngine::ComponentFactory::RegisterComponentOfType("NODE", []() {return std::make_shared<ComponentNode>();  });
+    MyEngine::ComponentFactory::RegisterComponentOfType("BACKGROUND", []() { return std::make_shared<ComponentBackground>(); });
+    MyEngine::ComponentFactory::RegisterComponentOfType("NUTNOTE", []() { return std::make_shared<ComponentNutNote>(); });
+    MyEngine::ComponentFactory::RegisterComponentOfType("PLAYER_CONTROLLER", []() { return std::make_shared<ComponentPlayerController>(); });
+>>>>>>> Stashed changes
 
-	engine.Init("data/scene.json");
-}
+    engine.Init("data/scene.json");
+    }
